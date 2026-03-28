8c81cab94992d073b9445404ce25421037acddd5
RahJess <jessicakantotiana@gmail.com>
2026-03-28 17:32:52 +0100

    feat: configure connection to database in DataSourceConfig relate to the SRP context

---
+++ src/main/java/DataSourceConfig.java
@@ -0,0 +1,15 @@
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
